0c3af873e7f6a3f1f1bee357f578a555ace1b06c
Daniel Martins <62864768+danismartins19@users.noreply.github.com>
2022-03-14 10:28:13 -0300

    redes sociais adicionadas

---
+++ script.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.conteudo-principal-escrito-titulo', {duration: 1500});
+sr.reveal('.conteudo-principal-escrito-subtitulo', {duration: 2000});
+sr.reveal('.conteudo-principal-escrito-botao', {duration: 2000});
+
+sr.reveal('.conteudo-secundario', {duration: 1500});
+
+sr.reveal('.sobre-titulo', {duration: 1500});
+sr.reveal('.sobre-paragrafo', {duration: 1500});
+
+
+
+sr.reveal('.projeto-banner', {duration: 1500});
+sr.reveal('.projeto-banner-inv', {duration: 1500});
+
+sr.reveal('.social-descricao', {duration: 1500});
